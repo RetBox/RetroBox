0df769885957dacb2dba9298da3bea81a8c6467f
xafero <georg.berendt@outlook.com>
2023-06-07 22:01:58 +0200

    Added app img handling

--- RetroBox.Update/ZipLoader.cs
+++ RetroBox.Update/ZipLoader.cs
@@ -11,6 +11,8 @@ namespace RetroBox.Update
 {
     public static class ZipLoader
     {
+        private const string AppImgExt = ".AppImage";
+
         public static async Task<string> UnCompress(string inputFile, string outPath,
             ProgressHandler receive, CancellationToken token)
         {
@@ -18,6 +20,12 @@ namespace RetroBox.Update
                 return outPath;
             if (!Directory.Exists(outPath))
                 Directory.CreateDirectory(outPath);
+
+            if (inputFile.EndsWith(AppImgExt))
+            {
+                outPath = await LinkAppImage(inputFile, outPath, receive);
+                return outPath;
+            }
 
             using var zipFile = new ZipFile(inputFile);
 
@@ -56,6 +64,28 @@ namespace RetroBox.Update
                 var interval = TimeSpan.FromSeconds(1);
                 StreamUtils.Copy(stream, fileOut, buffer, receive, interval, holder, rawName);
             }
+            return outPath;
+        }
+
+        private static async Task<string> LinkAppImage(string inputFile, string outPath, ProgressHandler receive)
+        {
+            const int bufferSize = 4096;
+            var buffer = new byte[bufferSize];
+
+            var rawName = Path.GetFileName(inputFile);
+            var info = new FileInfo(inputFile);
+            var holder = new ZipHolder { UncompressedSize = info.Length, ProcessedBytes = 0L };
+            var outName = Path.Combine(outPath, rawName);
+
+            await using var fileIn = File.OpenRead(inputFile);
+            await using var fileOut = File.Create(outName);
+            var interval = TimeSpan.FromSeconds(1);
+            StreamUtils.Copy(fileIn, fileOut, buffer, receive, interval, holder, rawName);
+
+            var linkName = rawName.Split('-', 2).First();
+            var outLink = Path.Combine(outPath, linkName);
+            File.CreateSymbolicLink(outLink, outName);
+
             return outPath;
         }
     }
