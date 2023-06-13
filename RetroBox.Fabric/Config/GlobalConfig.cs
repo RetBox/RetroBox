e77744fe40c8560db0ec28ce69348fc83302bcd4
xafero <georg.berendt@outlook.com>
2023-06-13 18:51:58 +0200

    Added not null

--- RetroBox.Fabric/Config/GlobalConfig.cs
+++ RetroBox.Fabric/Config/GlobalConfig.cs
@@ -4,8 +4,8 @@ namespace RetroBox.Fabric.Config
 {
     public class GlobalConfig : IGlobalConfig
     {
-        public Dictionary<string, EmuExe> InstalledEmu { get; set; }
+        public Dictionary<string, EmuExe>? InstalledEmu { get; set; }
         
-        public Dictionary<string, string> InstalledRom { get; set; }
+        public Dictionary<string, string>? InstalledRom { get; set; }
     }
 }
